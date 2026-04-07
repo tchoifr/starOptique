116d1fff2d27361ea1e47b388aec42b341c2b3c6
GIORGI Francois <francois.GIORGI@uca.fr>
2026-04-07 14:35:29 +0200

    Update script source in index.html to new version

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
+import { Buffer } from 'buffer';
 import App from './App.vue';
 import router from './router/index.js';
 import './assets/styles.css';
+
+if (!globalThis.Buffer) {
+  globalThis.Buffer = Buffer;
+}
 
 createApp(App).use(router).mount('#app');
